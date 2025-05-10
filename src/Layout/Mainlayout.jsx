81c290875a3b1426ad2495540e9c97e32e84936d
Al Mahmud Badhon <engi.badhon@gmail.com>
2025-05-10 07:54:29 +0600

    final

--- src/Layout/Mainlayout.jsx
+++ src/Layout/Mainlayout.jsx
@@ -1,12 +1,22 @@
+import Faq from "../Components/Faq/Faq";
+import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header";
 import Herosection from "../Components/Hero/Herosection";
+import Pricing from "../Components/Pricing.jsx/Pricing";
 import Service from "../Components/Service/Service";
+import Testimonial from "../Components/Testimonial/Testimonial";
+import Trail from "../Components/Trail/Trail";
 export default function Mainlayout() {
   return (
     <>
       <Header></Header>
       <Herosection></Herosection>
       <Service></Service>
+      <Testimonial></Testimonial>
+      <Pricing></Pricing>
+      <Faq></Faq>
+      <Trail></Trail>
+      <Footer></Footer>
     </>
   );
 }
